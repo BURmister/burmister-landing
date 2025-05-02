0c64b411540feee68537af1741576f0fda8df476
BURmister <olegcernysov903@gmail.com>
2025-05-02 12:24:25 +0300

    update

--- src/shared/animation/index.jsx
+++ src/shared/animation/index.jsx
@@ -1,3 +1,3 @@
-export { default as AnimationFade } from './Fade';
+export { default as AnimationFade } from './Fade1';
 export { default as AnimationRotate1 } from './Rotate1';
 export { default as AnimationRotate2 } from './Rotate2';
